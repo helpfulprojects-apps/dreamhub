e8a44ca19c6fe4bfe843aac7222b54db21df9e71
dreamhub <freedreamprojects@gmail.com>
2026-01-31 11:13:20 -0600

    commit

--- apps/invite/config.js
+++ apps/invite/config.js
@@ -1,4 +1,4 @@
 window.DH = window.DH || {};
 window.DH.base = "/dreamhub";
-window.DH.inviteBase = "/dreamhub/apps/invite"; // optional but makes links predictable
-window.DH.inviteApi = "https://script.google.com/macros/s/AKfycbzxYMfNVlELc00Nzcowt3wGSHUHGZ_k3JGIAQk6JDsLCpu7C10FxTtYnWD0FeyXUiEf/exec";
+window.DH.inviteBase = "/dreamhub/apps/invite";
+window.DH.inviteApi = "https://script.google.com/macros/s/AKfycbxt-bhE6TILOUOGY1o1Vn2AuiPFPI2RVMxl1iiKGnAeLfMKgIKShZL6BPlxeBgd986y/exec";
